305815eb7e0154fdf8880345235e2e352d06dec7
Raphael Freudiger <laser_b@gmx.ch>
2017-05-07 08:37:50 +0200

    feat: add support for nested embedded objects

--- js/views/embedded.js
+++ js/views/embedded.js
@@ -1,19 +1,41 @@
 var Embedded = Backbone.View.extend({
+    initialize: function(options) {
+        this.number = options.number || 0;
+        this.parent = options.parent || "embedded";
+    },
+
     className: 'embedded panel panel-default',
 
     template: _.template($('#embedded_template').html()),
 
-    number: 0,
-
     render: function() {
         var that = this;
-        var embedded = this.model.embedded.data;
+        var embedded;
+        var keys;
+        if( this.model.embedded.data ) {
+            embedded = this.model.embedded.data;
+        } else {
+            embedded = [];
+            keys = Object.keys(this.model.embedded);
+            $.each(keys, function(i, obj) {
+                if(Array.isArray(that.model.embedded[obj])) {
+                    $.each(that.model.embedded[obj], function(j, item) {
+                        embedded.push(item);
+                    });
+                } else {
+                    embedded.push(that.model.embedded[obj]);
+                }
+            });
+        }
         var data = [];
         $.each(embedded, function(i, obj) {
             that.$el.append(Mustache.render(that.template(),
                         {
+                embName: keys ? keys[i] : "",
                 name: obj.props.name || "",
-                index: i
+                index: i,
+                number: that.number,
+                parent: that.parent
             }
             ));
             if(obj.props) {
@@ -26,6 +48,14 @@ var Embedded = Backbone.View.extend({
                     selfName: 'open'
                 });
                 that.$el.find(".emb_links").last().html(objLinks.render().el);
+            }
+            if(obj.embedded && Object.keys(obj.embedded).length) {
+                var objEmbedded = new Embedded({
+                    model: obj,
+                    number: i+1,
+                    parent: 'none'
+                });
+                that.$el.find(".emb_embedded").last().html(objEmbedded.render().el);
             }
         });
         return this;
