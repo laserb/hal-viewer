7531ff63081b5ac547ab352d7ec2dda99438a9f3
Raphael Freudiger <laser_b@gmx.ch>
2017-05-11 05:44:43 +0200

    fix: hide login when clicking outside the form

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ var router = new Router({
 Backbone.history.start();
 
 // Get the modal
-var modal = document.getElementById('id01');
+var loginModal = document.getElementById('login-form');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == loginModal) {
+        loginModal.style.display = "none";
     }
 };
 
